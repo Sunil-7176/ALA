ea6a4177ac60b23d0448dc33c44b51c3f99fed3d
Sunil <74315053+Sunil-7176@users.noreply.github.com>
2023-08-19 17:31:40 +0530

    Add files via upload

--- Assignment_03/movie_review.py
+++ Assignment_03/movie_review.py
@@ -1,49 +1,65 @@
+# Import pandas and numpy
 import pandas as pd
 import numpy as np
 
-# Read the dataset
-data = pd.read_csv('moviereviews.csv')
+# Read the data set
+df = pd.read_csv("moviereviews.csv")
 
-# Calculate movie review vectors
-movie_reviews = data.pivot_table(index='movie', columns='review', aggfunc='size', fill_value=0)
-movie_vectors = movie_reviews.values
+# Define a function to calculate the movie review vector
+def movie_vector(movie):
+  # Get the reviews for the movie
+  reviews = df[df["movie"] == movie]["review"]
+  # Initialize an empty vector of length 26 (one for each letter)
+  vector = np.zeros(26)
+  # Loop through the reviews
+  for review in reviews:
+    # Convert the review to lower case and remove punctuation
+    review = review.lower().translate(str.maketrans("", "", ".,;:'\"?!"))
+    # Loop through the letters in the review
+    for letter in review:
+      if "a" <= letter <= "z":
+    # Increment the corresponding element in the vector by 1
+        vector[ord(letter) - ord("a")] += 1
+  # Return the vector
+  return vector
 
-# Find similarity using dot product and angle between vectors
-def cosine_similarity(v1, v2):
-    dot_product = np.dot(v1, v2)
-    norm_v1 = np.linalg.norm(v1)
-    norm_v2 = np.linalg.norm(v2)
-    epsilon = 1e-8
-    if norm_v1 == 0 or norm_v2 == 0:
-        return 0
-    
-    cosine_sim = dot_product / (norm_v1 * norm_v2)
-    return cosine_sim
+# Define a function to calculate the similarity between two vectors using dot product and angle formulas
+def similarity(v1, v2):
+  # Calculate the dot product of the vectors
+  dot = np.dot(v1, v2)
+  # Calculate the magnitude of each vector
+  mag1 = np.sqrt(np.sum(v1**2))
+  mag2 = np.sqrt(np.sum(v2**2))
+  # Calculate the cosine of the angle between the vectors
+  cos = dot / (mag1 * mag2)
+  # Return the similarity score (higher is more similar)
+  return cos
 
-num_movies = movie_vectors.shape[0]
-similarities = np.zeros((num_movies, num_movies))
+# Get the unique movies in the data set
+movies = df["movie"].unique()
 
-print("Movie Vectors:")
-print(movie_vectors)
+# Initialize an empty list to store the pairs of movies and their similarity scores
+pairs = []
 
-for i in range(num_movies):
-    for j in range(i, num_movies):
-        similarity = cosine_similarity(movie_vectors[i], movie_vectors[j])
-        similarities[i, j] = similarity
-        similarities[j, i] = similarity
+# Loop through all possible pairs of movies
+for i in range(len(movies)):
+  for j in range(i + 1, len(movies)):
+    # Get the movie names
+    movie1 = movies[i]
+    movie2 = movies[j]
+    # Calculate their vectors
+    v1 = movie_vector(movie1)
+    v2 = movie_vector(movie2)
+    # Calculate their similarity score
+    score = similarity(v1, v2)
+    # Append the pair and the score to the list
+    pairs.append((movie1, movie2, score))
 
-print("Similarities:")
-print(similarities)
+# Sort the list by descending order of similarity score
+pairs.sort(key=lambda x: x[2], reverse=True)
 
-# Identify top 3 pairs of movies that are very much alike
-top_pairs = []
-for i in range(num_movies):
-    for j in range(i+1, num_movies):
-        top_pairs.append((data['movie'][i], data['movie'][j], similarities[i, j]))
-
-top_pairs.sort(key=lambda x: x[2], reverse=True)
-top_pairs = top_pairs[:3]
-
-for pair in top_pairs:
-    print(f"Movies: {pair[0]}, {pair[1]}, Similarity: {pair[2]:.4f}")
+# Print the top 3 pairs of movies that are very much alike
+print("Top 3 pairs of movies that are very much alike:")
+for i in range(3):
+  print(f"{i + 1}. {pairs[i][0]} and {pairs[i][1]} with similarity score of {pairs[i][2]:.4f}")
 
